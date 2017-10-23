621ddf5de5ebb1759da2cfb37b1d4c7f42f38ea1
Saga-Chan <louise.crampon@gmail.com>
2017-10-23 11:10:03 +0200

    Add files via upload

--- Syntaxe.c
+++ Syntaxe.c
@@ -8,11 +8,11 @@ Tree creerNode(typeToken t, Tree fd, Tree fg)
 
 	//printf("Je suis dans creerNode\n");
 	Tree arbre = (Tree) malloc(sizeof(Node));
-	
+
 	arbre->tok = t;
 	arbre->pTokBfor = fd;
 	arbre->pTokNext = fg;
-	
+
 	return arbre;
 }
 
@@ -20,53 +20,55 @@ Tree Synt(typeToken t[], int * i)
 {
 	Tree arbre = NULL;
 	Tree tmp = NULL;
-	
+
 	while(t[*i].lexem != END)
 	{
-	
+
 		printf("i vauts: %d\n", *i);
 		printf("Le lexem vaut %d\n", t[*i].lexem);
-		
+
 		switch(t[*i].lexem)
 		{
 			//printf("Je suis dans le switch\n");
 			case VAR: case REAL:
 			{
 				printf("Je suis dans real/var\n");
-				
+
 				arbre = creerNode(t[*i], NULL, NULL);
 
 				break;
 			}
 			case FUNCT:
 			{
-			
+
 				printf("Je suis dans func\n");
-				
-				int * tmp_var = i;
-				
-				*tmp_var = *tmp_var + 2;
-				tmp = Synt(t, tmp_var);
-				
-				arbre = creerNode(t[*i], tmp, NULL);
-				
+
+				int tmp_var = *i;
+                int tmpp_var = *i;
+
+				tmp_var = tmp_var + 2;
+				tmp = Synt(t, &tmp_var);
+
+				return creerNode(t[tmpp_var], tmp, NULL);
+
 				break;
 			}
 			case OPERATOR:
 			{
 				printf("Je suis dans operator\n");
-				
+
 				int * tmp_var = i;
-				
+
 				*tmp_var += 1;
-				
-				arbre = creerNode(t[*i], arbre, Synt(t, tmp_var));
-				
+
+				return creerNode(t[*i], arbre, Synt(t, tmp_var));
+
 				break;
 			}
 			default: break;
 		}
-		(*i)++;
+		if(t[*i].lexem != END){(*i)++;}
+
 	}
 			printf(" en dehors du while: i vauts: %d\n", *i);
 		printf(" en dehors du while: Le lexem vaut %d\n", t[*i].lexem);
