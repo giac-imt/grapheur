e2355a56874ba75408ea2f77cd0a4e2d7eab0c32
giac-imt <32895137+giac-imt@users.noreply.github.com>
2017-10-24 13:49:19 +0200

    MAJ lex.c

--- lex.c
+++ lex.c
@@ -5,48 +5,74 @@
 #include "lex.h"
 
 int lex(char str[], typeToken t[]){
+    //i avance dans la saisie user, j dans le tableau typeToken
     int i=0, j= 0;
-    while(str[i] != '\0'){
-        switch(tolower(str[i])){
 
-            case '+' : t[j].lexem = OPERATOR;
+    //Différencier la barre ouvrante de la barre fermante
+    int barre = 0;
+
+    while(str[i] != '\0'){
+        switch(str[i]){
+
+            case '+' :
+                t[j].lexem = OPERATOR;
                 t[j].valor.ope = PLUS;
                 break;
 
-            case '-' : t[j].lexem = OPERATOR;
+            case '-' :
+                t[j].lexem = OPERATOR;
                 t[j].valor.ope = MINUS;
                 break;
 
-            case '*' : t[j].lexem = OPERATOR;
+            case '*' :
+                t[j].lexem = OPERATOR;
                 t[j].valor.ope = MULTIP;
                 break;
 
-            case '/' : t[j].lexem = OPERATOR;
+            case '/' :
+                t[j].lexem = OPERATOR;
                 t[j].valor.ope = DIV;
                 break;
 
-            case '^' : t[j].lexem = OPERATOR;
+            case '^' :
+                t[j].lexem = OPERATOR;
                 t[j].valor.ope = POW;
                 break;
 
-            case '(' : t[j].lexem = PAR_OP;
+            case '(' :
+                t[j].lexem = PAR_OP;
                 break;
 
-            case ')' : t[j].lexem = PAR_CL;
+            case ')' :
+                t[j].lexem = PAR_CL;
                 break;
 
-            case 'x' : t[j].lexem = VAR;
-            break;
+            case '|' :
+                if(barre == 0){
+                    t[j].lexem = BAR_OP;
+                    barre = 1;
+                } else {
+                    t[j].lexem = BAR_CL;
+                    barre = 0;
+                }
+                break;
+
+            case 'x' :
+                t[j].lexem = VAR;
+                break;
 
             default :
-                if((str[i]<'a') && (str[i]>'z')){
-                    //erreur
+                if((str[i]>'a') && (str[i]<'z')){
+                    //Impossible de rentrer dans ce if
+                    printf("Alphabet\n");
                 }
-                if((str[i]<'0') && (str[i]>'9')){
-                    //erreur
+                if((str[i]>'0') && (str[i]<'9')){
+                    //Ni dans celui-là
                 }
                 else {
-                    //Error
+                    t[j].lexem = ERROR;
+                    t[j].valor.err.code = 101;
+                    sprintf(t[j].valor.err.message, "Aucune correspondance trouvée\n");
                 }
                 break;
         }
@@ -55,7 +81,7 @@ int lex(char str[], typeToken t[]){
     }
     t[j].lexem = END;
     t[j].valor.err.code = 100;
-    sprintf(t[j].valor.err.message, "Success");
+    sprintf(t[j].valor.err.message, "Success\n");
     return 0;
 }
 
